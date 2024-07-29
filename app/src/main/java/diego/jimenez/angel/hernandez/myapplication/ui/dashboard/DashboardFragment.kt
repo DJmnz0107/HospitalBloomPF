7fd22d307d44cf87f547733611dbe9cc762fb2ce
Angel <20210086@ricaldone.edu.sv>
2024-07-28 20:58:43 -0600

    haciendo los inserts

--- app/src/main/java/diego/jimenez/angel/hernandez/myapplication/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/diego/jimenez/angel/hernandez/myapplication/ui/dashboard/DashboardFragment.kt
@@ -1,13 +1,21 @@
 package diego.jimenez.angel.hernandez.myapplication.ui.dashboard
 
+import Modelo.ClaseConexion
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import diego.jimenez.angel.hernandez.myapplication.R
 import diego.jimenez.angel.hernandez.myapplication.databinding.FragmentDashboardBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class DashboardFragment : Fragment() {
 
@@ -27,6 +35,30 @@ class DashboardFragment : Fragment() {
 
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
+        val textoEnfermedad = root.findViewById<TextView>(R.id.mtEnfermedad)
+
+        val enviarEnfermedad = root.findViewById<Button>(R.id.btnIngresarEnfermedad)
+
+        enviarEnfermedad.setOnClickListener {
+            try {
+                CoroutineScope(Dispatchers.IO).launch {
+                    val objConexion = ClaseConexion().cadenaConexion()
+
+                   val agregarEnfermedad = objConexion?.prepareStatement("insert into Enfermedades(nombre_enfermedad) values(?)")!!
+                    agregarEnfermedad.setString(1,textoEnfermedad.text.toString())
+                    agregarEnfermedad.executeUpdate()
+                    withContext(Dispatchers.Main){
+                        textoEnfermedad.setText("")
+                        Toast.makeText(requireContext(), "La enfermedad se ha agregado correctamente",Toast.LENGTH_SHORT).show()
+                    }
+
+                }
+            }catch (e:Exception){
+                println("El error es $e")
+            }
+        }
+
 
 
         return root
--- app/src/main/java/diego/jimenez/angel/hernandez/myapplication/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/diego/jimenez/angel/hernandez/myapplication/ui/notifications/NotificationsFragment.kt
@@ -1,16 +1,23 @@
 package diego.jimenez.angel.hernandez.myapplication.ui.notifications
 
+import Modelo.ClaseConexion
 import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import diego.jimenez.angel.hernandez.myapplication.R
 import diego.jimenez.angel.hernandez.myapplication.databinding.FragmentNotificationsBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.util.Calendar
 
 class NotificationsFragment : Fragment() {
@@ -31,6 +38,31 @@ class NotificationsFragment : Fragment() {
 
         _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
+        val textoHabitacion = root.findViewById<TextView>(R.id.mtHabitacion)
+        val enviarHabitacion = root.findViewById<Button>(R.id.btnIngresarHabitacion)
+
+        enviarHabitacion.setOnClickListener {
+            try {
+                CoroutineScope(Dispatchers.IO).launch {
+                     val objConexion = ClaseConexion().cadenaConexion()
+
+                    val agregarHabitacion = objConexion?.prepareStatement("insert into Habitaciones(num_habitacion) values(?) ")!!
+                    agregarHabitacion.setString(1,textoHabitacion.text.toString())
+                    agregarHabitacion.executeUpdate()
+                    withContext(Dispatchers.Main){
+                        textoHabitacion.setText("")
+                        Toast.makeText(requireContext(), "La habitacion se ha agregado correctamente", Toast.LENGTH_SHORT).show()
+
+                    }
+                }
+            }catch (e:Exception){
+                println("El error es $e")
+            }
+        }
+
+
+
 
 
         return root
